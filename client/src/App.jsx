fbd982513199ed13c077908f587252de46c2113a
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2024-03-02 18:58:42 +0700

    installing package react-router-dom also adding routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
 export default function App() {
-  return <h1 className="text-2xl font-[600] text-blue-400">MERN blog</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="text-2xl font-[600] text-blue-400">Home</div>;
+}
